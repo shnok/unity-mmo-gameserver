e8b8cfb810bed81acc5f9f6e67cd2391082ed248
Shnok <skyler.maki@gmail.com>
2022-06-13 20:09:23 +0200

    add assets

---
+++ unity-client/Assets/OldSchool/EntityAnimationOld.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EntityAnimationOld : MonoBehaviour {
+#if false
+	[HideInInspector]
+	public Animator animator;
+	public EntityStatus status;
+	public NetworkSyncAnim syncAnim;
+
+	// Use this for initialization
+	void Start () {
+		status = GetComponent<EntityStatus> ();
+		syncAnim = GetComponent<NetworkSyncAnim> ();
+		animator = GetComponent<Animator>();
+
+		if(animator== null) {
+			animator = GetComponentInChildren<Animator> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(status.state == MonsterState.Runing) {
+			SetBool ("Run", true);
+		} else {
+			SetBool ("Run", false);
+		}
+
+		if(status.state == MonsterState.Waiting) {
+			SetBool ("Wait", true);
+		} else {
+			SetBool ("Wait", false);
+		}
+
+		if(status.state == MonsterState.Walking) {
+			SetBool ("Walk", true);
+		} else {
+			SetBool ("Walk", false);
+		}
+
+		if(status.state == MonsterState.Attacking) {
+			SetBool ("Attack", true);
+		} else {
+			SetBool ("Attack", false);
+		}
+
+		if(status.state == MonsterState.Dead) {
+			SetBool ("Dead", true);
+		} else {
+			SetBool ("Dead", false);
+		}
+	}
+
+	void SetBool(string name, bool value) {
+		if (animator.GetBool (name) != value) {
+			animator.SetBool (name, value);
+			syncAnim.EmitAnimatorInfo (name, value.ToString());
+		}
+	}
+
+	void SetFloat(string name, float value) {
+		if (Mathf.Abs(animator.GetFloat (name) - value) > 0.2f) {
+			animator.SetFloat (name, value);
+			syncAnim.EmitAnimatorInfo (name, value.ToString());
+		}
+	}
+#endif
+}
--- unity-client/Assets/Scripts/Controller/PlayerController.cs
+++ unity-client/Assets/Scripts/Controller/PlayerController.cs
@@ -13,25 +13,26 @@ public class PlayerController : MonoBehaviour {
 	public float _currentSpeed;
 	public float defaultSpeed = 4;
 	private Vector2 _axis;
+	public bool canMove = true;
 
-	/* Gravity */ 
+	/* Gravity */
 	private float _verticalVelocity = 0;
 	public float _jumpForce = 10;
 	public float _gravity = 28;
 
 	private NetworkTransform networkTransform;
 	public static PlayerController _instance;
-    public static PlayerController GetInstance() {
-        return _instance;
-    }
+	public static PlayerController GetInstance() {
+		return _instance;
+	}
 
-    void Awake() {
-        if (_instance == null) {
-            _instance = this;
-        }
-    }
+	void Awake() {
+		if(_instance == null) {
+			_instance = this;
+		}
+	}
 
-	void Start () {
+	void Start() {
 		controller = GetComponent<CharacterController>();
 		networkTransform = GetComponent<NetworkTransform>();
 	}
@@ -40,61 +41,61 @@ public class PlayerController : MonoBehaviour {
 		_axis = new Vector2(x, y);
 	}
 
-	void FixedUpdate () {
-        /* Speed */
-        _currentSpeed = GetMoveSpeed (_currentSpeed);
+	void FixedUpdate() {
+		/* Speed */
+		_currentSpeed = GetMoveSpeed(_currentSpeed);
 
-        /* Angle */
-		_finalAngle = GetRotationValue (_finalAngle);
-        transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.Euler (Vector3.up * _finalAngle), Time.deltaTime * 7.5f);
+		/* Angle */
+		_finalAngle = GetRotationValue(_finalAngle);
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Vector3.up * _finalAngle), Time.deltaTime * 7.5f);
 
-        /* Direction */
-		moveDirection = GetMoveDirection (moveDirection, _currentSpeed);
-        controller.Move (moveDirection * Time.deltaTime);
+		/* Direction */
+		moveDirection = GetMoveDirection(moveDirection, _currentSpeed);
+		controller.Move(moveDirection * Time.deltaTime);
 	}
 
 	private float GetRotationValue(float angle) {
 		float startAngle = angle;
-		if (KeyPressed()) {
-            angle = Mathf.Atan2 (_axis.x, _axis.y) * Mathf.Rad2Deg;
-            angle = Mathf.Round (angle / 45f);
-            angle *= 45f;
-            angle += Camera.main.transform.eulerAngles.y;
-		}	
+		if(KeyPressed()) {
+			angle = Mathf.Atan2(_axis.x, _axis.y) * Mathf.Rad2Deg;
+			angle = Mathf.Round(angle / 45f);
+			angle *= 45f;
+			angle += Camera.main.transform.eulerAngles.y;
+		}
 
 		if(startAngle != angle && networkTransform.isActiveAndEnabled) {
 			networkTransform.ShareRotation(angle);
 		}
 
-        return angle;	
+		return angle;
 	}
 
 	private Vector3 GetMoveDirection(Vector3 direction, float speed) {
-        Vector3 forward = Camera.main.transform.TransformDirection (Vector3.forward);
-        forward.y = 0;
-        Vector3 right = new Vector3 (forward.z, 0, -forward.x);
+		Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
+		forward.y = 0;
+		Vector3 right = new Vector3(forward.z, 0, -forward.x);
 
 		/* Handle gravity */
-		if (controller.isGrounded) {
+		if(controller.isGrounded) {
 			if(_verticalVelocity < -1.25f) {
-				_verticalVelocity = -1.25f; 	
+				_verticalVelocity = -1.25f;
 			}
 		} else {
-			_verticalVelocity -= _gravity * Time.deltaTime; 
+			_verticalVelocity -= _gravity * Time.deltaTime;
 		}
 
 		/* Handle input direction */
-		if (KeyPressed () && controller.isGrounded) {
-			direction = _axis.x * right + _axis.y * forward;		
-		} else if (!controller.isGrounded) {
+		if(KeyPressed() && controller.isGrounded) {
+			direction = _axis.x * right + _axis.y * forward;
+		} else if(!controller.isGrounded) {
 			direction = transform.forward;
 		}
 
-		direction = direction.normalized * speed;	
-		
-		direction.y = _verticalVelocity;	
+		direction = direction.normalized * speed;
 
-        return direction;
+		direction.y = _verticalVelocity;
+
+		return direction;
 	}
 
 	private float GetMoveSpeed(float speed) {
@@ -103,19 +104,27 @@ public class PlayerController : MonoBehaviour {
 		if(KeyPressed()) {
 			speed = defaultSpeed;
 		} else if(speed > 0 && controller.isGrounded) {
-			speed -= (defaultSpeed/smoothDuration) * Time.deltaTime;
+			speed -= (defaultSpeed / smoothDuration) * Time.deltaTime;
 		}
 
-        return speed < 0 ? 0 : speed;
+		return speed < 0 ? 0 : speed;
 	}
 
-	private bool KeyPressed() {
-        return _axis.x != 0 || _axis.y != 0;
+	public bool KeyPressed() {
+		return _axis.x != 0 || _axis.y != 0;
 	}
 
 	public void Jump() {
 		if(controller.isGrounded) {
 			_verticalVelocity = _jumpForce;
-		}		
+		}
+	}
+
+	public void LookForward(string type) {
+		if(type == "camera") {
+			_finalAngle = Camera.main.transform.eulerAngles.y;
+		}
+
+		transform.rotation = Quaternion.Euler(Vector3.up * _finalAngle);
 	}
 }
